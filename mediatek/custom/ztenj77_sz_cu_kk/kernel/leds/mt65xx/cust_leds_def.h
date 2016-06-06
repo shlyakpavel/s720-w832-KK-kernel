cc18638b2b718b237bb91247caaa536d3fbae8df
pavel <shlyak-pavel2011@ya.ru>
2016-06-06 21:52:50 +0400

    Remove directories

--- mediatek/custom/ztenj77_sz_cu_kk/kernel/leds/mt65xx/cust_leds_def.h
+++
@@ -1,7 +0,0 @@
-#ifndef _CUST_LEDS_DEF_H
-#define _CUST_LEDS_DEF_H
-
-//#define CUST_LEDS_BACKLIGHT_PMIC_PARA /* parallel */
-//#define CUST_LEDS_BACKLIGHT_PMIC_SERI /* series */
-typedef int (*cust_set_brightness)(int level);
-#endif /* _CUST_LEDS_DEF_H */
